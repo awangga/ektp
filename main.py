fda6735cb59fc3b85bc5f97cbc8d1406fbbacbe3
Rolly Maulana Awangga <rolly@awang.ga>
2018-04-25 22:43:32 +0700

    add template

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from lib import cilok
+#from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return "Welcome to Indonesia Electronic KTP Middleware"
+	#return render_template('index.html')
 
 	
 @app.route("/<gurih>")
